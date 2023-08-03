f7057460d8d34cecd4537910f49b5dbb6db35406
Gorchakov Ilya (develinux) <motohusa@mail.ru>
2023-08-03 22:16:53 +0300

    remove comment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-//test
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
